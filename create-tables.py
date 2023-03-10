5b387d7da45df626ab844171a2b7c7912840a122
beddo018 <beddo018@umn.edu>
2023-03-10 12:20:16 +0900

    added docstrings; altered drop statements to allow initial execution

--- create-tables.py
+++ create-tables.py
@@ -2,20 +2,37 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+'''
+The functions in this file execute their namesake grouped queries in 'sql_queries.py'.
+Their primary purpose is to create the staging tables and the five denormalized tables
+within the Redshift cluster created via psycopg2 Python shell for AWS.
+'''
 
 def drop_tables(cur, conn):
+
+    '''
+    Drops all the tables created by the subsequent create_tables function.
+    Its placement allows the file to be run multiple times for experimentation purposes.
+    '''
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Creates the two staging tables and the five denormalized (star schema) OLAP tables.
+    '''
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Uses the variables created in 'dwh.cfg' to connect to the Redshift cluster, then invokes the above function 
+    vis a vis the cursor object created by psycopg2. 
+    '''
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- sql_queries.py
+++ sql_queries.py
@@ -11,13 +11,13 @@ log_json = config.get('S3', 'LOG_JSONPATH')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE staging_events"
-staging_songs_table_drop = "DROP TABLE staging_songs"
-songplay_table_drop = "DROP TABLE songplays"
-user_table_drop = "DROP TABLE users"
-song_table_drop = "DROP TABLE songs"
-artist_table_drop = "DROP TABLE artists"
-time_table_drop = "DROP TABLE time"
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS song_plays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
